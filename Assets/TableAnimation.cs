e8a482ffe731c7cd1dc969a65f0e6e18a9ae7de3
Croach <44575914+xCroach@users.noreply.github.com>
2023-11-14 16:33:43 +0100

    Milestone 2
    -created a lever and buttons
    -implemented first animation and trigger
    -fixed a few bugs

---
+++ Assets/TableAnimation.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TableAnimation : MonoBehaviour
+{
+
+    private Animator anim;
+    private Boolean isTriggered;
+    public GameObject lever;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lever = GameObject.Find("Lever");
+        Debug.Log(lever.GetComponent<HingeJoint>().angle);
+
+        if (lever.GetComponent<HingeJoint>().angle <= -40)
+        {
+            anim.SetBool("isAnimating",true);
+        }
+        
+    }
+}
