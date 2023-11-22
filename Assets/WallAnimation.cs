34a1c681cae30478b2d2a3d44b5d4b4526a2b688
Croach <44575914+xCroach@users.noreply.github.com>
2023-11-22 15:06:54 +0100

    Animation in first mockup
    -added animated wall in first mockup of escape room
    -added origin for better orientation

---
+++ Assets/WallAnimation.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallAnimation : MonoBehaviour
+{
+
+    public Animator animator;
+    public GameObject lever;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //lever = GameObject.Find("LeverMiddle");
+        //Debug.Log(lever.GetComponent<HingeJoint>().angle);
+        
+        if (lever.GetComponent<HingeJoint>().angle <= -40 || lever.GetComponent<HingeJoint>().angle >= 40)
+        {
+            animator.SetBool("wallDown",true);
+        }
+        else
+        {
+            animator.SetBool("wallDown",false);
+        }
+        
+    }
+}
