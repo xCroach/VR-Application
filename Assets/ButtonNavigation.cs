40fbce39faa4f960a582d1bbb4889235cdf634ca
Croach <44575914+xCroach@users.noreply.github.com>
2024-01-24 12:18:02 +0100

    Added Main Menu UI and logic
    -How to play not yet implemented (blank canvas)

---
+++ Assets/ButtonNavigation.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.SceneManagement;
+
+public class ButtonNavigation : MonoBehaviour
+{
+    [SerializeField] private GameObject menu;
+    [SerializeField] private GameObject how;
+    public void StartGame()
+    {
+        SceneManager.LoadSceneAsync("EscapeRoom");
+    }
+
+    public void HowToPlay()
+    {
+        menu.SetActive(false);
+        how.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Back()
+    {
+        how.SetActive(false);
+        menu.SetActive(true);
+    }
+}
