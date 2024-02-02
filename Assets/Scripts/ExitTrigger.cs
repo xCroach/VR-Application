c5a7ed02fbb78fa262371520014bcc67a624dc51
Croach <44575914+xCroach@users.noreply.github.com>
2024-02-02 13:53:04 +0100

    Added Opening in Third room and ladder for exit

---
+++ Assets/Scripts/ExitTrigger.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("ExitEnter");
+        SceneManager.LoadSceneAsync("MainMenu");
+    }
+}
--- Assets/Scripts/Finish_Sound.cs
+++ Assets/Scripts/Finish_Sound.cs
@@ -6,12 +6,14 @@ using UnityEngine;
 public class Finish_Sound : MonoBehaviour
 {
     private bool alreadyTriggered = false;
+    [SerializeField] private GameObject exit;
     private void OnTriggerEnter(Collider other)
     {
         if (!alreadyTriggered)
         {
             AkSoundEngine.SetState("music", "finish");
             AkSoundEngine.PostEvent("Play_win", gameObject);
+            exit.SetActive(true);
             alreadyTriggered = true;
         }
         
